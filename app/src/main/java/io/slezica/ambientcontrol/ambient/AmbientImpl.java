a24e4c30e7759f7a4bdbc3c3b767de3d0d716be4
Santiago Lezica <salezica@gmail.com>
2022-09-01 18:58:50 -0300

    Update to androidx

--- app/src/main/java/io/slezica/ambientcontrol/ambient/AmbientImpl.java
+++ app/src/main/java/io/slezica/ambientcontrol/ambient/AmbientImpl.java
@@ -5,8 +5,10 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.provider.Settings;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import io.slezica.ambientcontrol.utils.TaggedLog;
 
 public class AmbientImpl implements Ambient {
